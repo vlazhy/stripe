b097af5a09fe8267096d4ab2c0c445c2c8f0116b
vlazhy <vla.zhy.work@gmail.com>
2025-10-25 23:01:13 +0300

    Delete api/test-ping.js

--- api/test-ping.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-  return res.status(200).json({ 
-    status: 'OK',
-    timestamp: new Date().toISOString(),
-    method: req.method,
-    body: req.body,
-    message: 'Backend is alive!'
-  });
-}
